c02923a5e5cf0e070c128975108acca2ae74240c
ElsaintAl <154474807+ElsaintAl@users.noreply.github.com>
2024-09-19 16:38:19 +0300

    Update venizelos_spata.py

--- Weather/venizelos_spata.py
+++ Weather/venizelos_spata.py
@@ -2,6 +2,8 @@ import calendar
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from matplotlib.ticker import PercentFormatter
 
 from to_SI import new_si
 from date import DateGenerator
@@ -10,9 +12,9 @@ from date import DateGenerator
 def main():
     filename = 'spata_venizelos_2024_5.csv'
     dg = DateGenerator(filename)
-    multi_histogram(filename)
+#    multi_histogram(filename, dg)
 #    temp_humidity_corr(filename)
-#    month_temp_l_plot(filename, dg)
+    month_temp_l_plot(filename, dg)
 
 
 def create_df(filename):
@@ -79,7 +81,8 @@ def get_pressure(filename):
 def month_temp_l_plot(filename, date_generator):
     """
     Generate the required data and plots the high vs. low temperatures for a specif month.
-    :param filename: The cvs file.
+    :param filename: The cvs file name for data generation. The name format is
+                     'spata_venizelos_"year_number"_"month_number".csv'
     :param date_generator: The date generator class.
     """
 
@@ -89,13 +92,14 @@ def month_temp_l_plot(filename, date_generator):
     plt.style.use('classic')
 
     fig, ax = plt.subplots()
-    ax.plot(dates, h_temp, c='red', alpha=0.5)
-    ax.plot(dates, l_temp, c='blue', alpha=0.5)
-    plt.fill_between(dates, h_temp, l_temp, facecolor='blue', alpha=0.1)
+    ax.plot(dates, h_temp, c='r', alpha=0.5, label='High Temp')
+    ax.plot(dates, l_temp, c='b', alpha=0.5, label='Low Temp')
+    plt.fill_between(dates, h_temp, l_temp, facecolor='#21918c', alpha=0.1)
 
     # Format plot.
+    plt.ylim(min(l_temp)-1, max(h_temp)+1)
     plt.grid(visible=True)
-    plt.title(f'Daily high and lows temperatures, {get_month_name(month)} {year}', fontsize=24)
+    plt.title(f'Daily high and lows temperatures for {get_month_name(month)} {year} in Spata', fontsize=24)
     plt.xlabel('', fontsize=16)
     fig.autofmt_xdate()
     plt.ylabel('Temperature (°C)', fontsize=16)
@@ -123,10 +127,10 @@ def temp_humidity_corr(filename):
                          'Low Humility (%)': a_humi})
     h_df = pd.DataFrame({'High Temperature (°C)': a_temp,
                          'High Humility (%)': a_humi})
-#    sns.set(style="ticks", color_codes=True)
-#    l_g = sns.pairplot(l_df)
-#    a_g = sns.pairplot(a_df)
-#    h_g = sns.pairplot(h_df)
+    #    sns.set(style="ticks", color_codes=True)
+    #    l_g = sns.pairplot(l_df)
+    #    a_g = sns.pairplot(a_df)
+    #    h_g = sns.pairplot(h_df)
 
     plt.hist(l_temp, bins=31, edgecolor='black')
     plt.xlabel('Temperature')
@@ -136,13 +140,20 @@ def temp_humidity_corr(filename):
     plt.show()
 
 
-def multi_histogram(filename):
+def multi_histogram(filename, date_generator):
     """
     Creates a histogram graph for the average temperature, humidity, pressure and dew point
-    :param filename:
+
+    :param filename: The cvs file name for data generation. The name format is
+                   'spata_venizelos_"year_number"_"month_number".csv'
+    :param date_generator: The date generator class.
     """
+    dg = date_generator
+    month, year = dg.get_month_year()
     a_temp = get_temp(filename)[1]
     a_humi = get_humidity(filename)[1]
+    a = range(int(min(a_humi)), int(max(a_humi)))
+
     a_pres = get_pressure(filename)[1]
     a_dew = get_dew_point(filename)[1]
     df = pd.DataFrame({'Temperature (°C)': a_temp[1:],
@@ -151,18 +162,67 @@ def multi_histogram(filename):
                        'Dew Point (°C)': a_dew[1:]})
 
     figure, axis = plt.subplots(2, 2)
+    plt.suptitle(f'Distribution of weather parameters for {get_month_name(month)} {year} in Spata')
+    colors = sns.color_palette("viridis")
+    print(len(colors))
 
-    axis[0, 0].hist(df['Temperature (°C)'], bins=15, edgecolor='black')
-    axis[0, 0].set_title("Temperature (°C)")
+    for ax in axis.flatten():  # Flatten the array to iterate over individual Axes objects
+        if ax == axis[0, 0]:
+            bin_width = 0.5
+            sns.histplot(data=df, x='Temperature (°C)', kde=True, stat="probability", color=colors[2],
+                         label="Probabilities", binwidth=bin_width, ax=ax)
+            ax2 = ax.twinx()
+            sns.kdeplot(data=df, x='Temperature (°C)', color="k", label="KDE density", ls=':', lw=2, ax=ax2)
+            ax2.set_ylim(0, ax.get_ylim()[1] / bin_width)  # similar limits on the y-axis to align the plots
+            ax2.yaxis.set_major_formatter(
+                PercentFormatter(1 / bin_width))  # show an axis such that 1/bin_width corresponds to 100%
+            ax2.set_ylabel(f'Probability for a bin width of {bin_width}')
+            ax.legend(loc='upper left')
+            ax2.legend(loc='upper right')
 
-    axis[0, 1].hist(a_humi, bins=15, edgecolor='black')
-    axis[0, 1].set_title("Humidity (%)")
-    axis[1, 0].hist(a_pres, bins=15, edgecolor='black')
-    axis[1, 0].set_title("Pressure (hPc)")
+        if ax == axis[0, 1]:
+            bin_width = 2
+            sns.histplot(data=df, x='Humidity (%)', kde=True, stat="probability", color=colors[2],
+                         label="Probabilities", binwidth=bin_width, ax=ax)
+            ax2 = ax.twinx()
+            sns.kdeplot(data=df, x='Humidity (%)', color="k", label="kde density", ls=':', lw=2, ax=ax2)
+            ax2.set_ylim(0, ax.get_ylim()[1] / bin_width)  # similar limits on the y-axis to align the plots
+            ax2.yaxis.set_major_formatter(
+                PercentFormatter(1 / bin_width))  # show an axis such that 1/bin_width corresponds to 100%
+            ax2.set_ylabel(f'Probability for a bin width of {bin_width}')
+            ax.legend(loc='upper left')
+            ax2.legend(loc='upper right')
 
-    axis[1, 1].hist(df['Dew Point (°C)'], bins=15, edgecolor='black')
-    axis[1, 1].set_title("Dew Point (°C)")
-    df['Dew Point (°C)'].plot.kde(zorder=2, color='C1')
+        if ax == axis[1, 0]:
+            # Get the bin edges
+            bin_edges = sns.histplot(data=df, x='Pressure (hPc)', kde=True, stat="probability", color=colors[2],
+                                     label="Probabilities", ax=ax).get_xticks()
+            # Calculate the bin width
+            bin_width = bin_edges[1] - bin_edges[0]
+
+            ax2 = ax.twinx()
+            sns.kdeplot(data=df, x='Pressure (hPc)', color="k", label="kde density", ls=':', lw=2, ax=ax2,
+                        bw_adjust=0.5, bw_method=1)
+            ax2.set_ylim(0, ax.get_ylim()[1] / bin_width)  # similir limits on the y-axis to align the plots
+            ax2.yaxis.set_major_formatter(
+                PercentFormatter(1 / bin_width))  # show axis such that 1/binwidth corresponds to 100%
+            ax2.set_ylabel(f'Probability for a bin width of {round(bin_width, 2)}')
+            ax.legend(loc='upper left')
+            ax2.legend(loc='upper right')
+
+        if ax == axis[1, 1]:
+            bin_width = 1
+            sns.histplot(data=df, x='Dew Point (°C)', kde=True, stat="probability", color=colors[2],
+                         label="Probabilities", binwidth=bin_width, ax=ax)
+            ax2 = ax.twinx()
+            sns.kdeplot(data=df, x='Dew Point (°C)', color="k", label="kde density", ls=':', lw=2, ax=ax2)
+            ax2.set_ylim(0, ax.get_ylim()[1] / bin_width)  # similar limits on the y-axis to align the plots
+            ax2.yaxis.set_major_formatter(
+                PercentFormatter(1 / bin_width))  # show an axis such that 1/bin_width corresponds to 100%
+            ax2.set_ylabel(f'Probability for a bin width of {bin_width}')
+            ax.legend(loc='upper left')
+            ax2.legend(loc='upper right')
+    plt.tight_layout()
 
     plt.show()
 
